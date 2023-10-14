e916e749e0fd3b9bd97ddf31ef10df87f4749220
Igor Fomin <ifomin67@gmail.com>
2023-10-14 20:13:59 +0300

    Calculator

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+            double a;
+            double b;
+            double total;
+            char oper;
+
+            Console.WriteLine("Input first operand:");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Input operator:");
+            oper = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Input second operand:");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            if (oper == '+')
+            {
+                total = a + b;
+                Console.WriteLine(a + "+" + b + "=" + total);
+            }
+
+            else if (oper == '-')
+            {
+                total = a - b;
+                Console.WriteLine(a + "-" + b + "=" + total);
+            }
+
+            else if (oper == '*')
+            {
+                total = a * b;
+                Console.WriteLine(a + "*" + b + "=" + total);
+            }
+
+            else if (oper == '/')
+            {
+                total = a / b;
+                Console.WriteLine(a + "/" + b + "=" + total);
+            }
+            else
+            {
+                Console.WriteLine("Unknown operator");
+            } 
+    }
+}
